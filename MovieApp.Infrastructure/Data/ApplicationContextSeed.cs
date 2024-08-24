a18c19b6cf2a77b5c72d6c5c84e0d4ac9642a3b6
Alexandr Goldin <50864552+AlexandrGoldin@users.noreply.github.com>
2024-08-24 14:26:14 +0300

    Update ApplicationContextSeed.cs

--- MovieApp.Infrastructure/Data/ApplicationContextSeed.cs
+++ MovieApp.Infrastructure/Data/ApplicationContextSeed.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using MovieApp.Infrastructure.Entities;
+using MovieApp.ApplicationCore.Entities;
 
 namespace MovieApp.Infrastructure.Data
 {
