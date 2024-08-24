2a84f497f6ec98256ba91e21007c3bcc0e047738
Alexandr Goldin <50864552+AlexandrGoldin@users.noreply.github.com>
2024-08-24 14:20:08 +0300

    Update IReadRepository.cs

--- MovieApp.ApplicationCore/Interfaces/IReadRepository.cs
+++ MovieApp.ApplicationCore/Interfaces/IReadRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Ardalis.Specification;
 
-namespace MovieApp.Infrastructure.Interfaces
+namespace MovieApp.ApplicationCore.Interfaces
 {
     public interface IReadRepository<T> : IReadRepositoryBase<T> where T : class, IAggregateRoot
     {
