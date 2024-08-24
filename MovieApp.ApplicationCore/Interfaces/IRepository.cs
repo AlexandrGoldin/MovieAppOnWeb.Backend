d9d6b80cffa8ee0b77e599097b93db9ab377c66f
Alexandr Goldin <50864552+AlexandrGoldin@users.noreply.github.com>
2024-08-24 14:20:30 +0300

    Update IRepository.cs

--- MovieApp.ApplicationCore/Interfaces/IRepository.cs
+++ MovieApp.ApplicationCore/Interfaces/IRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Ardalis.Specification;
 
-namespace MovieApp.Infrastructure.Interfaces
+namespace MovieApp.ApplicationCore.Interfaces
 {
     public interface IRepository<T> : IRepositoryBase<T> where T : class, IAggregateRoot
     {
